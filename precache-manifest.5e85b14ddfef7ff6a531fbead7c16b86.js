33718bc24ffd5b135b6b0687f629f542e9f21ef9
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-08-20 23:02:44 +0800

    Updates

---
+++ precache-manifest.5e85b14ddfef7ff6a531fbead7c16b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d06c8e054cb0624ec5557db8e781671",
+    "url": "/invoice_test-task/index.html"
+  },
+  {
+    "revision": "895d7d8d220fc3e2ed26",
+    "url": "/invoice_test-task/static/css/main.18690778.chunk.css"
+  },
+  {
+    "revision": "11c028a4300190291a97",
+    "url": "/invoice_test-task/static/js/2.0291534d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/invoice_test-task/static/js/2.0291534d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895d7d8d220fc3e2ed26",
+    "url": "/invoice_test-task/static/js/main.4db7bcde.chunk.js"
+  },
+  {
+    "revision": "db0b2b41d074394458be",
+    "url": "/invoice_test-task/static/js/runtime-main.987ef47d.js"
+  }
+]);
